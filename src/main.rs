eb0bb89bbb238944ace837aee0140288776cdb60
Trevor Smith <trevorsm7@gmail.com>
2020-12-19 17:06:34 -0800

    Add mem+bus unit test

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,18 @@ use bus::Bus;
 use memory::RAM;
 
 fn main() {
+}
+
+#[test]
+fn test_mem_bus() {
     let mut bus = Bus::<u16, u8>::new();
 
+    let addr = 100;
     let ram = RAM::<u16, u8>::new(1);
-    bus.add_port(ram.map_cb(100), ram.read_cb(), ram.write_cb());
+    bus.add_port(ram.map_cb(addr), ram.read_cb(), ram.write_cb());
 
-    bus.write(100, 5);
-    println!("{}", bus.read(100).unwrap());
-    println!("{}", ram.read(0));
+    let value = 5;
+    bus.write(addr, value);
+    assert_eq!(bus.read(addr).unwrap(), value);
+    assert_eq!(ram.read(0), value);
 }
