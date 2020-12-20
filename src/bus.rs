96fefa8570717b07cdf2e5e852b369262282b1dd
Trevor Smith <trevorsm7@gmail.com>
2020-12-19 21:22:49 -0800

    Check bus port ranges for overlap

--- src/bus.rs
+++ src/bus.rs
@@ -1,4 +1,5 @@
 use std::sync::{Arc, RwLock};
+use std::ops::Range;
 
 use num_traits::PrimInt;
 
@@ -7,43 +8,60 @@ pub trait Port<Address, Data> {
     fn write(&mut self, address: Address, data: Data);
 }
 
+type PortPtr<Address, Data> = Arc<RwLock<dyn Port<Address, Data>>>;
+
 struct PortMap<Address, Data> {
-    range: (Address, Address),
-    port: Arc<RwLock<dyn Port<Address, Data>>>,
+    range: Range<Address>,
+    port: PortPtr<Address, Data>,
 }
 
 pub struct Bus<Address, Data> {
     ports: Vec<PortMap<Address, Data>>,
 }
 
-impl<Address: PrimInt, Data: Copy> Bus<Address, Data> {
+impl<Address: PrimInt + std::fmt::Debug, Data: Copy> Bus<Address, Data> {
     pub fn new() -> Self {
         Self { ports: vec![] }
     }
 
-    pub fn add_port(&mut self, range: (Address, Address), port: Arc<RwLock<dyn Port<Address, Data>>>) {
-        // TODO sort by start and check for overlapping ranges
-        self.ports.push(PortMap { range, port });
+    // TODO return an error instead of panicking
+    pub fn add_port(&mut self, range: Range<Address>, port: PortPtr<Address, Data>) {
+        match self.ports.binary_search_by_key(&range.start, |port| port.range.start) {
+            Err(index) => {
+                if index > 0 && self.ports[index - 1].range.end > range.start {
+                    panic!(format!("address range {:?} overlaps with {:?}", range, self.ports[index - 1].range));
+                }
+                if index < self.ports.len() && self.ports[index].range.start < range.end {
+                    panic!(format!("address range {:?} overlaps with {:?}", range, self.ports[index].range));
+                }
+                self.ports.insert(index, PortMap { range, port });
+            },
+            Ok(index) => panic!(format!("address range {:?} overlaps with {:?}", range, self.ports[index].range)),
+        }
     }
 
-    pub fn write(&self, address: Address, data: Data) {
-        // NOTE this can write to multiple ports that claim the same address
-        for port in self.ports.iter() {
-            if address >= port.range.0 && address < port.range.1 {
-                let address = address - port.range.0;
-                port.port.write().unwrap().write(address, data);
-            }
+    fn map_address(&self, address: Address) -> Option<(&PortPtr<Address, Data>, Address)> {
+        match self.ports.binary_search_by_key(&address, |port| port.range.start) {
+            Ok(index) => Some((&self.ports[index].port, Address::zero())),
+            Err(index) => {
+                if index > 0 {
+                    let prev = &self.ports[index - 1];
+                    if prev.range.contains(&address) {
+                        return Some((&prev.port, address - prev.range.start));
+                    }
+                }
+                None
+            },
         }
+    }
+
+    pub fn write(&self, address: Address, data: Data) -> Option<()> {
+        self.map_address(address).map(|(port, address)|
+            port.write().unwrap().write(address, data))
     }
 
     pub fn read(&self, address: Address) -> Option<Data> {
-        // NOTE this only reads from the first port that claims it; maybe make this an error since it doesn't match write?
-        for port in self.ports.iter() {
-            if address >= port.range.0 && address < port.range.1 {
-                let address = address - port.range.0;
-                return Some(port.port.read().unwrap().read(address));
-            }
-        }
-        None
+        self.map_address(address).map(|(port, address)|
+            port.read().unwrap().read(address))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,40 @@ fn main() {
 }
 
 #[test]
-fn test_mem_bus() {
+#[should_panic]
+fn test_mem_overlap_same() {
+    let ram = Arc::new(RwLock::new(RAM::<u16, u8>::new(10)));
+    let mut bus = Bus::<u16, u8>::new();
+    bus.add_port(0..10, ram.clone());
+    bus.add_port(0..10, ram.clone());
+}
+
+#[test]
+#[should_panic]
+fn test_mem_overlap_after() {
+    let ram = Arc::new(RwLock::new(RAM::<u16, u8>::new(10)));
+    let mut bus = Bus::<u16, u8>::new();
+    bus.add_port(0..10, ram.clone());
+    bus.add_port(9..19, ram.clone());
+}
+
+#[test]
+#[should_panic]
+fn test_mem_overlap_before() {
+    let ram = Arc::new(RwLock::new(RAM::<u16, u8>::new(10)));
+    let mut bus = Bus::<u16, u8>::new();
+    bus.add_port(10..20, ram.clone());
+    bus.add_port(1..11, ram.clone());
+}
+
+#[test]
+fn test_mem_rw() {
     let mut bus = Bus::<u16, u8>::new();
 
     let addr = 100;
     let size = 1;
     let ram = Arc::new(RwLock::new(RAM::<u16, u8>::new(size)));
-    bus.add_port((addr, addr+size), ram.clone());
+    bus.add_port(addr..addr+size, ram.clone());
 
     let value = 5;
     bus.write(addr, value);
